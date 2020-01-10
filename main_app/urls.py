b289ced4c4b29011a293d9136f75e4475c4e4b60
dimchoo <imdimchoo@gmail.com>
2020-01-10 13:44:50 +0300

    Views всег верхнего меню и шаблоны. Views основного меню, кроме брендов.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from main_app.views import HomeView, ProductView
+from main_app.views import *
 
 app_name = 'main_app'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('products/product/<int:pk>/', ProductView.as_view(), name='product')
+    path('sale/', SaleView.as_view(), name='sale'),
+    path('delivery/', delivery_view, name='delivery'),
+    path('contacts/', contacts_view, name='contacts'),
+    path('price-list/', price_list_view, name='price_list'),
+    path('products/product/<int:pk>/', ProductView.as_view(), name='product'),
+    path('products/all/', AllProductsView.as_view(), name='all_products'),
+    path('products/<slug>/', ProductsCategoryView.as_view(), name='products_category'),
+    path('brands/all/', BrandsView.as_view(), name='all_brands'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from main_app.models import Slide, Product, Brand, CommonPageDescription
+from main_app.models import Slide, Product, Brand, CommonPageDescription, ContactPhone, ContactEmail
 
 
 class HomeView(ListView):
@@ -29,3 +29,86 @@ class ProductView(DetailView):
             category=self.object.category
         ).order_by('?').exclude(pk=self.object.pk)[:4]
         return context
+
+
+class AllProductsView(ListView):
+    model = Product
+    paginate_by = 4
+    context_object_name = 'all_products_list'
+    template_name = 'main_app/products.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['page_descriptions'] = CommonPageDescription.objects.filter(page_name='Каталог')
+        return context
+
+
+class ProductsCategoryView(ListView):
+    model = Product
+    paginate_by = 4
+    template_name = 'main_app/products_category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['page_title'] = Product.objects.filter(
+            category__slug=self.kwargs['slug']
+        ).first().category.name
+        context['page_description'] = Product.objects.filter(
+            category__slug=self.kwargs['slug']
+        ).first().category.description
+        return context
+
+
+class SaleView(ListView):
+    model = Product
+    paginate_by = 4
+    template_name = 'main_app/sale.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(type='Sale').order_by('-pk')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['page_descriptions'] = CommonPageDescription.objects.filter(page_name='Распродажа')
+        return context
+
+
+def contacts_view(request):
+    page_descriptions = CommonPageDescription.objects.filter(page_name='Контакты')
+    contacts_phones = ContactPhone.objects.all()
+    contacts_emails = ContactEmail.objects.all()
+
+    context = {
+        'page_descriptions': page_descriptions,
+        'contacts_phones': contacts_phones,
+        'contacts_emails': contacts_emails,
+    }
+
+    return render(request, 'main_app/contacts.html', context)
+
+
+def delivery_view(request):
+    page_descriptions = CommonPageDescription.objects.filter(page_name='Доставка')
+
+    return render(request, 'main_app/delivery.html', {'page_descriptions': page_descriptions})
+
+
+def price_list_view(request):
+    price_list_emails = ContactEmail.objects.all()
+
+    return render(request, 'main_app/price-list.html', {'price_list_emails': price_list_emails})
+
+
+class BrandsView(ListView):
+    model = Brand
+    paginate_by = 6
+    template_name = 'main_app/brands.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['page_descriptions'] = CommonPageDescription.objects.filter(page_name='Бренды')
+        return context
+
