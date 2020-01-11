fac295d06f166061ad7a6480a617e014546b19ac
dimchoo <imdimchoo@gmail.com>
2020-01-12 01:44:42 +0300

    Добавлена возможность экспорта email'ов из админки.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,4 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from main_app.models import *
 
@@ -8,4 +9,8 @@ admin.site.register(Product)
 admin.site.register(CommonPageDescription)
 admin.site.register(ContactEmail)
 admin.site.register(ContactPhone)
-admin.site.register(UserEmail)
+
+
+@admin.register(UserEmail)
+class UserEmailAdmin(ImportExportModelAdmin):
+    pass
