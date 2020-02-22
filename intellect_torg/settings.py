2a1dd99588dadbffcf0c19ee6c061418ded11de0
dimchoo <imdimchoo@gmail.com>
2020-02-23 01:36:51 +0300

    Добавлен импорт локальных настроек

--- intellect_torg/settings.py
+++ intellect_torg/settings.py
@@ -117,9 +117,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
