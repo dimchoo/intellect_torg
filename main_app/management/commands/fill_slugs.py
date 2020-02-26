93fb890676d1b3bd9c120b1c595eb0476870fb31
dimchoo <imdimchoo@gmail.com>
2020-02-26 21:08:38 +0300

    Обновление скрипта заполнения slug-полей модели Product

--- main_app/management/commands/fill_slugs.py
+++ main_app/management/commands/fill_slugs.py
@@ -7,8 +7,17 @@ from main_app.models import Product
 class Command(BaseCommand):
     def handle(self, *args, **options):
         products = Product.objects.all()
+
+        alphabet = {
+            'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+            'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm',
+            'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+            'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+            'ы': 'i', 'э': 'e', 'ю': 'yu', 'я': 'ya'
+        }
+
         for product in products:
-            product.slug = slugify(product.name)
+            product.slug = slugify((''.join(alphabet.get(w, w) for w in str(product.name.lower()))))
             product.save()
 
         print('Done!')
